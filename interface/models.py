3f1a55fcb8496a5131777e28fc06894f97dd1c76
Admin <admin@LIB2-49.local>
2016-07-28 13:15:40 -0400

    Display friendly dates

--- interface/models.py
+++ interface/models.py
@@ -11,3 +11,22 @@ class Item(models.Model):
 	created = models.DateField()
 	isFinished = models.BooleanField()
 	finished = models.DateField()
+	@property
+	def displayDate(self):
+		interval = datetime.date.today() - self.created
+		if interval == datetime.timedelta(days = 0):
+			return 'today'
+		elif interval == datetime.timedelta(days = 1):
+			return 'yesterday'
+		elif interval < datetime.timedelta(days = 7):
+			return 'this week'
+		return str(self.created)
+
+
+
+
+
+
+
+
+
